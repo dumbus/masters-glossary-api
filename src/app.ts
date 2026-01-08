7f809a48e489d541050e2d8c27d630530be863cc
dumbus <dumbusx@gmail.com>
2026-01-09 00:02:37 +0300

    feat: use http in api code (https will be handled by vps nginx)

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,25 @@ import { errorHandler } from './middlewares/errorHandler';
 
 const app = express();
 
+// CORS middleware
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, OPTIONS, PATCH',
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+  );
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  next();
+});
+
 app.use(express.json());
 
 // Routes
